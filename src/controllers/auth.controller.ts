605afcc443d42b96de25ac0ed065fe3b097e1d1f
Mahmoud ben Romdhane <mahmoudbenromdhane407@gmail.com>
2022-12-02 06:48:24 +0100

    add readme

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,5 +3,5 @@ import { NextFunction, Response, Router, Request } from 'express';
 const catchAsync = require('../utils/catchAsync');
 
 const register = catchAsync(async (req : Request, res : Response, next : NextFunction) => {
-    
-  });
+      
+  }); 
